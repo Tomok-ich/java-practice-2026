c0e7d6efd58ea78a3f72df356e5ad03240538ab7
Tomok-ich <garoforce999sssgod@gmail.com>
2026-07-01 14:45:12 +0300

    Выполнено 2 задание: реализовано изменение описания профиля с помощью метода update

---
+++ Task2/src/ru/itis/shop/user/api/UserConsoleOperations.java
@@ -0,0 +1,106 @@
+package ru.itis.shop.user.api;
+
+import ru.itis.shop.user.application.UserService;
+import ru.itis.shop.user.domain.User;
+
+import java.util.Scanner;
+
+public class UserConsoleOperations {
+
+    private final UserService userService;
+    private final Scanner scanner;
+
+    public UserConsoleOperations(UserService userService) {
+        this.userService = userService;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showMenu() {
+        printUserMenu();
+
+        String command = scanner.nextLine();
+
+        switch (command) {
+            case "1": {
+                signUp();
+            }
+            break;
+            case "2": {
+                signIn();
+            }
+            break;
+            case "3": {
+                findById();
+            }
+            break;
+            case "4": {
+                updateProfileDescription();
+            }
+            break;
+            case "0": {
+                System.exit(0);
+            }
+        }
+    }
+
+
+    private static void printUserMenu() {
+        System.out.println("1. Регистрация пользователя");
+        System.out.println("2. Вход в систему");
+        System.out.println("3. Найти пользователя по id");
+        System.out.println("4. Обновить описание профиля");
+        System.out.println("0. Выход");
+    }
+
+    private void signUp() {
+        System.out.println("Сейчас будем регистрировать пользователя");
+        System.out.println("Введите email:");
+        String email = scanner.nextLine();
+        System.out.println("Введите password:");
+        String password = scanner.nextLine();
+        System.out.println("Введите описание профиля:");
+        String profileDescription = scanner.nextLine();
+
+        userService.signUp(email, password, profileDescription);
+    }
+
+
+    private void signIn() {
+        System.out.println("Вы можете войти в приложение");
+        System.out.println("Введите email:");
+        String email = scanner.nextLine();
+        System.out.println("Введите password:");
+        String password = scanner.nextLine();
+
+        if (userService.signIn(email, password)) {
+            System.out.println("Вы вошли в приложение");
+        } else {
+            System.out.println("Email или пароль не верны");
+        }
+    }
+
+    private void findById() {
+        System.out.println("Введите id пользователя:");
+        String id = scanner.nextLine().trim();
+        User user = userService.findById(id);
+        if (user == null) {
+            System.out.println("Пользователь с таким id не найден");
+        } else {
+            System.out.println("Email пользователя: " + user.getEmail());
+        }
+    }
+
+    private void updateProfileDescription() {
+        System.out.println("Введите email пользователя:");
+        String email = scanner.nextLine();
+        System.out.println("Введите новое описание профиля:");
+        String profileDescription = scanner.nextLine();
+        if (userService.updateProfileDescription(email, profileDescription)) {
+            System.out.println("Данные обновлены");
+        } else {
+            System.out.println("Пользователь не найден");
+        }
+    }
+
+
+}
