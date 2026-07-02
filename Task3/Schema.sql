616ebd53e4e3ef293b027c4c53879b0024efa759
Tomok-ich <garoforce999sssgod@gmail.com>
2026-07-02 17:05:06 +0300

    Выполнено 3 задание: добавлен файл Schema.sql

--- Task3/Schema.sql
+++ Task3/Schema.sql
@@ -1,23 +1,23 @@
-select * from Product
-
-insert into Product (product_name, cost) values ('Pen', 50);
-
-insert into Product (product_name, cost) values ('Pen', 0,6);
-
-insert into Product (product_name, cost) values ('Pen', -45);
-
-select * from Product
-
-insert into Product (product_name, cost) values ('Paper', 0);
-
-insert into Product (product_name, cost) values ('Paper', 0,7);
-
-insert into Product (product_name, cost) values ('PC', 175000);
-
-select * from Product
-
 create table Product(
 					id serial unique not null,
 					product_name char(40) not null,
 					cost integer check(cost >= 0)
 );
+
+select * from Product
+
+insert into Product (product_name, cost) values ('PC', 175000);
+
+insert into Product (product_name, cost) values ('Paper', 0,7);
+
+insert into Product (product_name, cost) values ('Paper', 0);
+
+select * from Product
+
+insert into Product (product_name, cost) values ('Pen', -45);
+
+insert into Product (product_name, cost) values ('Pen', 0,6);
+
+insert into Product (product_name, cost) values ('Pen', 50);
+
+select * from Product
